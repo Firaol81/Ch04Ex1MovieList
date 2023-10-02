3c3a3b0711c8d72bb989bf12315c15981ec5f092
Firaol Baneta <firaolbaneta99@gmail.com>
2023-10-01 20:25:38 -0500

    FIRAOL

--- MovieList/Controllers/HomeController.cs
+++ MovieList/Controllers/HomeController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieList.Models;
 
 namespace MovieList.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MovieContext _context;
+
+        public HomeController(MovieContext ctx)
+        {
+            _context = ctx;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            // Retrieve a list of movies, including their associated genres, ordered by name.
+            var movies = _context.Movies
+                .Include(m => m.Genre) // Include the Genre navigation property.
+                .OrderBy(m => m.Name)  // Order the movies by name.
+                .ToList();             // Convert the result to a list.
+
+            // Pass the list of movies to the "Index" view.
+            return View(movies);
         }
     }
 }
--- MovieList/Models/Movie.cs
+++ MovieList/Models/Movie.cs
@@ -1,6 +1,23 @@
-﻿namespace MovieList.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieList.Models
 {
     public class Movie
     {
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "The movie name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The release year is required.")]
+        [Range(1900, 2100, ErrorMessage = "Please enter a valid release year.")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "The rating is required.")]
+        [Range(1, 5, ErrorMessage = "The rating must be between 1 and 5.")]
+        public int Rating { get; set; }
+
+        // You can add more properties as needed, such as GenreId, Director, Description, etc.
     }
 }
